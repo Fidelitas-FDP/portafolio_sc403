2c39ee86fe6674b76fa2c5a16f001fb8a85356b4
Fidelitas-FDP <evaristo11401@gmail.com>
2026-06-15 19:58:38 -0600

    Sincronizacion de portafolio con CLASE 2

---
+++ src/main/java/com/fdp/portafolio_sc403/PortafolioSc403Application.java
@@ -0,0 +1,13 @@
+package com.fdp.portafolio_sc403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioSc403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioSc403Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdp/portafolio_sc403/PortafolioSc403ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdp.portafolio_sc403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioSc403ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
